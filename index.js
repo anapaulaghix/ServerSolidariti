cf1e66770041efa034ca5f4f40525c552f6a638e
anapaula <anapaulaghis@gmail.com>
2022-10-08 11:19:17 -0300

    server

--- index.js
+++ index.js
@@ -13,32 +13,58 @@ const db = mysql.createPool({
 app.use(cors());
 app.use(express.json());
 
+//Controle de solicitacoes
+
 app.post("/register", (req, res) => {
   const {name} = req.body;
   const {email} = req.body;
   const {senha} = req.body;
+  const {quant} = req.body
 
-  let SQL = "INSERT INTO doadores (name, email, senha) VALUES (?, ?, ?)";
-  db.query(SQL, [name, email, senha], (err, result) => {
+  let mysql = "INSERT INTO doadores (name, email, senha, quant) VALUES (?, ?, ?, ?)";
+  db.query(mysql, [name, email, senha, quant], (err, result) => {
     if(err) console.log(err)
     else res.send(result);
   })
 })
 
 app.get("/pedido", (req, res) => {
-  let SQL = "SELECT * FROM doadores";
-  db.query(SQL, (err, result) => {
+  let mysql = "SELECT * FROM doadores";
+  db.query(mysql, (err, result) => {
     if(err) console.log(err)
     else res.send(result)
   })
 })
 
 app.delete("/delete/:id", (req,res) => {
-  const {id} = req.params;
-  let SQL = "DELETE FROM doadores WHERE iddoadores = ?";
-  db.query (SQL, [id], (err, result) => {
+  const {iddoadores} = req.params;
+  let mysql = "DELETE FROM doadores WHERE iddoadores = ?";
+  db.query (mysql, iddoadores, (err, result) => {
     if(err) console.log(err)
     else res.send(result);
+  })
+})
+
+//Contribuintes
+
+app.post("/cadastro", (req, res) => {
+  const {name} = req.body;
+  const {email} = req.body;
+  const {cidade} = req.body;
+  const {local} = req.body
+
+  let mysql = "INSERT INTO contribuintes (name, email, cidade, local) VALUES (?, ?, ?, ?)";
+  db.query(mysql, [name, email, cidade, local], (err, result) => {
+    if(err) console.log(err)
+    else res.send(result);
+  })
+})
+
+app.get("/cadastros", (req, res) => {
+  let mysql = "SELECT * FROM contribuintes";
+  db.query(mysql, (err, result) => {
+    if(err) console.log(err)
+    else res.send(result)
   })
 })
 
